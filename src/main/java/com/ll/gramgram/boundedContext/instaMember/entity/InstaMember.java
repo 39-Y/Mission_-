3e189db141c8e6b5dfe671be83d124271ff55f3d
Y39 <y39scode@gmail.com>
2023-04-12 15:10:16 +0900

    Refactor: Add relations InstaMember Entity

--- src/main/java/com/ll/gramgram/boundedContext/instaMember/entity/InstaMember.java
+++ src/main/java/com/ll/gramgram/boundedContext/instaMember/entity/InstaMember.java
@@ -1,12 +1,16 @@
 package com.ll.gramgram.boundedContext.instaMember.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -29,4 +33,13 @@ public class InstaMember {
     private String username;
     @Setter
     private String gender;
+    @OneToMany(mappedBy = "toInstaMember", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    @BatchSize(size = 1000)
+    @JsonIgnore
+    private List<LikeablePerson> likedBy;
+
+    @OneToMany(mappedBy = "fromInstaMember", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    @BatchSize(size = 100)
+    @JsonIgnore
+    private List<LikeablePerson> likes;
 }
